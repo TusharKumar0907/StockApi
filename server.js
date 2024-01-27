feddc6a900fc018189482055bada59c76d9510bb
TUSHAR <tktusharkumar12345@gmail.com>
2024-01-27 11:53:03 +0530

    Connected with Database

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello Tushar')
 })
 
-app.listen(3000);
+
+mongoose.connect('mongodb+srv://tk281141:8506938133@cluster0.j8pkvht.mongodb.net/ASSINGMENT2?retryWrites=true&w=majority')
+  .then(() => { 
+    console.log('Database Connected!') ;
+    app.listen(3000, ()=>{
+        console.log("Server is running on port 3000");
+    });
+})
+  .catch((error) => { console.log(error) } );
